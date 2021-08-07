d663b86aa085aed4ac8e63c1271f087fcbf86dd1
Augustine Ofoegbu <augustineofoegbu1@gmail.com>
2021-08-07 23:57:31 +0100

    Code snippets to illustrate different functions in React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function ListItem(props){
+  return <li>{props.value}</li>;
+} 
+function NumberList(props){
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => 
+  <ListItem key={number.toString()} value={number}/>
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  )
+}
+
+
+
+export default NumberList;
---
+++ src/blog.js
@@ -0,0 +1,32 @@
+function Post(props){
+    
+}
+
+function Blog(props){
+const sidebar = (
+    <ul>
+        {props.posts.map((post) => 
+            <li key={post.id}>
+                {post.title}
+            </li>
+        )}
+    </ul>
+);
+const content = props.posts.map((post) => 
+  <div key={post.id}>
+    <h3>{post.title}</h3>
+    <h3>{post.content}</h3>
+  </div>          
+);
+return (
+    <div>
+        {sidebar}
+        <hr />
+        {content}
+    </div>
+)
+}
+
+
+
+export default Blog;
